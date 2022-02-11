f0bf695f5ca72598d178dcb2e25011eb28d7736e
hernandw <hernandw@gmail.com>
2022-02-11 16:10:05 -0300

    modificacion de ruta home

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
-router.get("/products", productController.home);
+router.get("/", productController.home);
 
 
 router.get('/products/:id', productController.busqueda);
